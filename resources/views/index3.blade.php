e5e0be1c82cf5edfe9a64e5df5f9ac70051d7020
syllvianadiaa <syllvianadiaa@gmail.com>
2026-05-25 12:08:30 +0700

    Pertemuan_13

---
+++ resources/views/index3.blade.php
@@ -0,0 +1,41 @@
+@extends('template')
+@section('title','Data Pegawai')
+@section('konten')
+	<a href="/pegawai/tambah" class="btn btn-primary"> + Tambah Pegawai Baru</a>
+
+	<br/>
+	<br/>
+    <p>Cari Data Pegawai :</p>
+	<form action="/pegawai/cari" method="GET" class="form-inline">
+        <div class="form-group">
+		<input type="text" name="cari" placeholder="Cari Pegawai .." class"form-control">
+		<input type="submit" value="CARI" class="btn btn-light">
+        </div>
+	</form>
+
+	<br/>
+
+	<table class="table table-striped table-hover">
+		<tr>
+			<th>Nama</th>
+			<th>Jabatan</th>
+			<th>Umur</th>
+			<th>Alamat</th>
+			<th>Opsi</th>
+		</tr>
+		@foreach($pegawai as $p)
+		<tr>
+			<td>{{ $p->pegawai_nama }}</td>
+			<td>{{ $p->pegawai_jabatan }}</td>
+			<td>{{ $p->pegawai_umur }}</td>
+			<td>{{ $p->pegawai_alamat }}</td>
+			<td>
+				<a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-warning">Edit</a>
+
+				<a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-danger"> Hapus</a>
+			</td>
+		</tr>
+		@endforeach
+	</table>
+    {{$pegawai->links()}}
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,47 +3,76 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DosenController;
 use App\Http\Controllers\pegawaiDBController;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
 Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('welcome', function () {
     return view('welcome');
 });
 
 Route::get('halo', function () {
-	return "<h1>Halo, Selamat datang</h1> di tutorial laravel <b>www.malasngoding.com</b>";
+	return "<h1> Halo, Selamat datang</h1> di tutorial laravel <b>www.malasngoding.com</b>";
 });
 
 Route::get('blog', function () {
 	return view('blog');
 });
 
-Route::get('pert5', function () {
+Route::get('pertemuan5', function () {
 	return view('pertemuan5');
 });
 
-Route::get('pert1', function () {
-	return view('intro');
+Route::get('linktree', function () {
+	return view('linktree');
 });
 
-Route::get('pert2', function () {
-	return view('news1');
+Route::get('workshop', function () {
+	return view('workshop');
 });
 
-Route::get('pert3', function () {
+Route::get('news', function () {
+	return view('news');
+});
+
+Route::get('responsive', function () {
 	return view('responsive');
 });
 
-Route::get('pert4', function () {
-	return view('5026241160');
+Route::get('5026241010', function () {
+	return view('5026241010');
 });
 
-Route::get('tugas pertemuan 5', function () {
-	return view('tugas_pertemuan_5');
+Route::get('intro', function () {
+	return view('intro');
 });
 
-Route::get('/', function () {
-	return view('menu');
+Route::get('makeover', function () {
+	return view('makeover');
 });
 
-Route::get('dosen', [DosenController::class, 'index']);
-Route::get('biodata', [DosenController::class, 'biodata']);
+Route::get('dosen', [DosenController::class,'index']);
+
+Route::get('biodata', [DosenController::class,'biodata']);
+
+//crud pertemuan 11
 Route::get('/pegawai', [pegawaiDBController::class,'index']);
+Route::get('/pegawai/tambah', [pegawaiDBController::class,'tambah']);
+Route::post('/pegawai/store', [pegawaiDBController::class,'store']);
+Route::get('/pegawai/edit/{id}', [pegawaiDBController::class,'edit']);
+Route::post('/pegawai/update', [pegawaiDBController::class,'update']);
+Route::get('/pegawai/hapus/{id}', [pegawaiDBController::class,'hapus']);
+Route::get('/pegawai/cari', [pegawaiDBController::class,'cari']);
+
+//Pertemuan 7
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::get('/formulir', [PegawaiController::class,'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class,'proses']);
+
+// route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
